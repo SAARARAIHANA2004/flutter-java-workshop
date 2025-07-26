0cd5166f138c7bd7842323205d3613024047eb4c
SAARARAIHANA2004 <saararaihana@gmail.com>
2025-07-26 11:50:07 +0530

    Create TaskService.java

---
+++ TaskService.java
@@ -0,0 +1,32 @@
+package com.example.todo.service;
+
+import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task addTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public Task updateTask(Task updatedTask) {
+        return taskRepository.save(updatedTask);
+    }
+}
